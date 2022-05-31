0ec05b52073caf88ddfc195993fe2e523dfe20ef
laxman <laxmantandon@gmail.com>
2022-05-31 12:19:26 +0530

    initial commit

---
+++ src/server.js
@@ -0,0 +1,104 @@
+const http = require('http')
+const axios = require('axios')
+const express = require('express')
+const app = express()
+const server = http.createServer(app)
+
+const host = "0.0.0.0"
+const port = "35040"
+
+app.set('Host', host)
+app.set('Port', port)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => res.send('ESD App Running'))
+
+app.post('/esd', (req, res) => {
+
+    payload = req.body
+    items = payload.items_list
+
+    let item_array = []
+    for(const val of items) {
+        array_item = val.hscode + " " + val.stockitemname + " " + String(val.qty) + " " + String(val.rate) + " " + String(val.amt)
+        item_array.push(array_item)
+    }
+    payload.items_list = item_array
+
+    const options = {
+        headers: {
+            'Authorization': req.headers.authorization,
+            'Content-Type': 'application/json',
+            'Content-Length': JSON.stringify(payload).length
+        }
+    };
+
+    axios.post(req.headers.hostname, payload , options)
+    .then((x) => {
+        // console.log(x);
+        let str_data = JSON.stringify(x.data);
+        // console.log('str_data', str_data);
+        let result = str_data.replace(/\\/g,"");
+        console.log(result);
+        // console.log(result);
+        // res.setHeader('Content-Type', 'application/json');
+        res.send(result);
+    }).catch(ex => {
+        console.log(ex)
+        res.send(ex)
+    })
+
+
+    // esd_req_options = {
+    //     hostname: req.headers.hostname,
+    //     method: 'POST',
+    //     port: req.headers.hostport,
+    //     path: req.headers.hostpath,
+    //     agent: https.Agent({keepAlive:true}),
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'Authorization': req.headers.authorization,
+    //         'Content-Length': 400,
+    //         'WWW-Authenticate': 'Basic',
+    //         'Keep-Alive': 'timeout=5, max=5'
+    //     }
+    // }
+
+    // const esd_req = https.request(esd_req_options, esd_res => {
+        
+    //     // console.log(req.body)
+
+    //     esd_res.on('data', d => {
+    //         return d;
+    //     });
+
+    //     // esd_res.on('error', e => {
+    //     //     esd_req.write(JSON.stringify({'error': false, 'message': e}))
+    //     // });
+
+    //     if (esd_resp.statusCode == 200) {
+    //         esd_res.send(req.body)
+    //     }
+
+    // });
+
+    // esd_req.on('error', error => {
+    //     console.log('error', error)
+    // });
+
+    // esd_resp = esd_req.write(JSON.stringify(req.body))
+    // esd_req.end()
+
+    // res.send(esd_resp)
+
+})
+
+server.listen(port, host, () => {
+    console.log('Server Listening')
+})
+
+module.exports = {
+    app, server
+}
